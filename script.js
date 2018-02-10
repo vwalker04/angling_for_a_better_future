a4422175daa0c458a805796586671555a5f8d5ec
vwalker04 <vwalker04@yahoo.com>
2018-02-10 16:07:38 -0500

    Attempting to fix info window

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
   var mapOptions = {
-    center: new google.maps.LatLng(42.351422, -83.068341), zoom: 10,
+    center: new google.maps.LatLng(42.351422, -83.068341), zoom: 12,
     mapTypeId: 'hybrid' // Default is 'roadmap'
   };
   var map = new google.maps.Map(mapCanvas, mapOptions);
 
+  google.maps.event.addListener(map, 'click', function(event) {
+    var latLng = event.location;
+  });
+  var infowindow = new google.maps.InfoWindow({
+
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+
+
   // Adds marker
   google.maps.event.addListener(map, 'click', function(event) {
-     placeMarker(event.latLng);
+    placeMarker(event.latLng);
+    infowindow.open(map, marker);
   });
 
   function placeMarker(location) {
-      var marker = new google.maps.Marker({
-          position: location,
-          map: map
-      });
-
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      draggable: true
+    });
   }
+
 }
